150f16ee2a9eaf30319bd30b5ef69ac8f50fdfdc
HelcioVenancio <ghitflux@gmail.com>
2025-11-17 14:10:11 -0300

    fix: Adicionar TURBOPACK_ROOT ao script build

--- build.js
+++ build.js
@@ -10,14 +10,15 @@ const os = require('os');
 try {
   // Certificar que NODE_ENV está definido como production
   process.env.NODE_ENV = 'production';
+  process.env.TURBOPACK_ROOT = '/app';
 
   // Executar next build a partir do diretório do apps/web usando o executable correto
   const webDir = path.join(__dirname, 'apps/web');
   const nextBinary = path.join(__dirname, 'apps/web/node_modules/.bin/next');
-  const command = `cd "${webDir}" && "${nextBinary}" build`;
+  const command = `cd "${webDir}" && TURBOPACK_ROOT=/app "${nextBinary}" build`;
 
   console.log(`Executando: ${command}`);
-  execSync(command, { stdio: 'inherit', shell: '/bin/sh' });
+  execSync(command, { stdio: 'inherit', shell: '/bin/sh', env: process.env });
 
   process.exit(0);
 } catch (error) {
